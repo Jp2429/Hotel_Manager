27332a7a6deb78041edd2bd9dfa4bbbf55a0ae3e
John Panton <johnpanton29@outlook.com>
2023-02-02 09:48:38 +0000

    Added a select and also added input validation

--- client/src/components/BookingsForm.js
+++ client/src/components/BookingsForm.js
@@ -7,21 +7,53 @@ const BookingsForm = ({addBooking}) => {
         guestEmail: "",
         checkedIn: false    
     })
+    const [selectStatus,setStatus]=useState("")
+    
+
 
     const onChange = (evt) => {
         const newFormData = Object.assign({}, formData)
         newFormData[evt.target.name] = evt.target.value
         setFormData(newFormData)
     }
+    const onSelectChange =(evt)=>{
+        let newCheckedIn=false
+        const newFormData = Object.assign({}, formData)
+        if(evt.target.value==="true"){
+            newCheckedIn=true
+            newFormData[evt.target.name] = newCheckedIn
+            setStatus('true')
+            setFormData(newFormData)
+    
+        }
+        else if(evt.target.value==="false"){
+            newCheckedIn=false
+            newFormData[evt.target.name] = newCheckedIn
+            setStatus('false')
+            setFormData(newFormData)
+    
+        }
+        else{
+            return
+        }
+
+    }
 
     const onSubmit = (evt) => {
         evt.preventDefault()
-        addBooking(formData)
+        console.log(formData)
+        if(formData.guestName!=="" && formData.guestEmail!=="" && selectStatus!==""){
+            addBooking(formData)
+        }else{
+            alert("Please fill in all the fields")
+        }
+        
         setFormData({
             guestName: "",
-            guestEmail: "",
-            checkedIn: false
+            guestEmail: ""
+            // checkedIn: false
         })
+        setStatus("")
 
     }
 
@@ -30,6 +62,11 @@ const BookingsForm = ({addBooking}) => {
         <form className="form" onSubmit={onSubmit}>
             <input onChange={onChange} type="text" id="guestName" name="guestName" value={formData.guestName}/> 
             <input onChange={onChange} type="text" id="guestEmail" name="guestEmail" value={formData.guestEmail}/>
+            <select onChange={onSelectChange} name="checkedIn" value={selectStatus}>
+                <option value="" selected disabled>Is the guest checked in?</option>
+                <option value="true">True</option>
+                <option value="false">False</option>
+            </select>
             <input type="submit" value="Save"/> 
         </form>
     )
